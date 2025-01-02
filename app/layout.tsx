88896f745a77b22c1a03cd62bcfb952e1bc14c0b
suhong <1844966932@qq.com>
2025-01-02 17:59:56 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import {Toaster} from 'react-hot-toast';
 import "./globals.css";
+import {ThemeProviders} from '@/components/providers';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,8 +29,9 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+        <ThemeProviders>{children}</ThemeProviders>
         <Toaster/>
-        {children}
+        {/*{children}*/}
         </body>
         </html>
     );
