403152d94f576739def1748439913d42b20a2796
suhong <1844966932@qq.com>
2025-01-03 17:57:41 +0800

    update

--- app/backstage/layout.tsx
+++ app/backstage/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/backstage/Sidebar";
+import TopNavigation from "@/components/backstage/TopNavigation";
 
 export default function RootLayout({
                                        children,
@@ -10,8 +11,12 @@ export default function RootLayout({
         <>
             <div className="flex bg-[#f5f5f5]">
                 <Sidebar></Sidebar>
+                <div className="flex-1">
+                    <TopNavigation/>
+                    {children}
+                </div>
 
-                {children}
+
             </div>
 
         </>
---
+++ components/backstage/TopNavigation.tsx
@@ -0,0 +1,7 @@
+export default function TopNavigation() {
+    return (
+        <div className="w-full h-10 bg-white">
+            这是头部
+        </div>
+    )
+}
