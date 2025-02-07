8e473dfd206d798764e7e2230a5cbc843606c326
suhong <1844966932@qq.com>
2025-02-07 14:14:20 +0800

    模拟提交

--- app/backstage/category/page.tsx
+++ app/backstage/category/page.tsx
@@ -1,27 +1,25 @@
 import Card from "@/components/Card";
 import EditBlogDialog from "@/components/backstage/EditBlogDialog";
-import {getTags} from "@/action/tag"
-export const dynamic = 'force-dynamic'; // 禁用缓存
-export default async function Page() {
-    const list = await getTags()
-    console.log("list", list)
+import { getTags } from "@/action/tag";
+
+export default function Page({ list }) {
     return (
         <div className="p-4">
-            <EditBlogDialog/>
-
+            <EditBlogDialog />
             <div className="grid grid-cols-2 gap-6 md:grid-cols-5">
-                {
-                    list.map(item => {
-                        return (
-                            <div key={item.id}>
-                                <Card data={item}/>
-                            </div>
-                        )
-                    })
-                }
+                {list.map(item => (
+                    <div key={item.id}>
+                        <Card data={item} />
+                    </div>
+                ))}
             </div>
-
-
         </div>
-    )
+    );
+}
+
+export async function getServerSideProps() {
+    const list = await getTags();
+    return {
+        props: { list },
+    };
 }
